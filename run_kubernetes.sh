f43bb03456359ea390fe74c332967e817a55a303
Niyi Ajobiewe <adeniyi.ajobiewe@gmail.com>
2022-08-20 20:33:44 +0000

    created files to run docker and kube

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=neyobrainy/myfirstimage
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run myminikube \
+    --image=$dockerpath \
+    --port=80 \
+    -- labels app=myminikube
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward myminikube 8000:80
